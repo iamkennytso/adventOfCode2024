378a3fd9185f0b1b4e63305de3d8be4b8fc40cfd
Kenneth Tso <iamkennytso@gmail.com>
2024-12-17 03:45:27 -0500

    2b

--- 02b.js
+++ 02b.js
@@ -1,8 +1,8 @@
 const fs = require('fs').promises;
 
-const isValidLine = line => {
+const isValidLine = (line, hasLeeway = true) => {
   let prev = Number(line[0])
-  const isInc = prev < Number(line[1])
+  const isInc = prev < Number(line[line.length - 1])
 
   for (let i = 1; i < line.length; i++) {
     const num = line[i]
@@ -12,9 +12,15 @@ const isValidLine = line => {
       !isInc && (prev < num) ||
       Math.abs(prev - num) > 3
     ) {
-      return false
+      if (hasLeeway) {
+        const withoutPrev = [...line.slice(0, i-1), ...line.slice(i)]
+        const withoutCur = [...line.slice(0,i), ...line.slice(i + 1)]
+        return isValidLine(withoutCur, false) || isValidLine(withoutPrev, false)
+      } else {
+        return false
+      }
     }
-      prev = num
+    prev = num
   }
   return true
 }
@@ -24,6 +30,7 @@ const isValidLine = line => {
     const data = await fs.readFile('02a.txt', 'utf8');
     const lines = data.replaceAll('\r','').split('\n')
     let safes = 0
+
     lines.forEach(line => {
       const lineArr = line.split(' ').map(num => Number(num))
       if (isValidLine(lineArr)) safes++
